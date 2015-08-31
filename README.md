4efe92b403798ad16f10442cc71b4f0f32968ee4
thewolf <hatem.taleb@gmail.com>
2015-08-31 14:03:00 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# meteor-react-seed
+# meteor react seed
+
+
--- client/app.jsx
+++ client/app.jsx
@@ -11,7 +11,7 @@ App = React.createClass({
     render: function() {
         return (
             <div className="ui container">
-                <h3>Hello reactjs</h3>
+                <h3>Hello reactjs...</h3>
             </div>
         );
     }
